ea26cd829189629f10f6b5f080978a64c64612c0
CubicPython-Official <anonymouscubichacker@gmail.com>
2025-04-20 23:11:26 +0800

    Update GithubCredits.sh

--- GithubCredits.sh
+++ GithubCredits.sh
@@ -1,17 +1,17 @@
 read -p "Whats ur name ?:" name
 echo "Hello, $name nice to meet you!"
 sleep 2
-read -p "enter your github user:"
+read -p "enter your github user:" user
 echo "looking for '$user'.."
 sleep 7
 echo "'$user' Found!.."
 sleep 2
 read -p "Enter amount of credits for '$user':" credits
-echo ""$credits' Selected.."
+echo "$credits' Selected.."
 sleep 2
 echo "Looking for lost credits..."
 sleep 2
-echo "Found credits... This may take awhile..."
+echo "Found $credits lost credits... This may take awhile..."
 sleep 16
 echo "Successfully Gave $user $credits credits.."
 
